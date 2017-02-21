0394cf7f77282a4c459ec21a12d235fff056f8a0
Jose <joselmo@correo.ugr.es>
2017-02-21 23:10:11 +0100

    Añadidos los ejercicios del Tema 1 y  ejemplo Java 3D

--- Prácticas/tutoriales/Tutorial_1/src/tutorial_1/HelloJava3Da.java
+++ Prácticas/tutoriales/Tutorial_1/src/tutorial_1/HelloJava3Da.java
@@ -5,10 +5,20 @@
  */
 package tutorial_1;
 
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.SimpleUniverse;
 import java.applet.Applet;
 import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
-
+import java.awt.Label;
+import javax.media.j3d.Alpha;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.RotationInterpolator;
+import javax.media.j3d.TransformGroup;
 
 /**
  *
@@ -21,6 +31,7 @@ public class HelloJava3Da extends Applet {
         GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
         Canvas3D canvas3D = new Canvas3D(config);
         add("Center", canvas3D);
+        add("North",new Label("Esto es el top"));
 
         BranchGroup scene = createSceneGraph();
         scene.compile();
@@ -36,3 +47,31 @@ public class HelloJava3Da extends Applet {
         // end of HelloJava3Da (constructor)
 
     }
+    
+    public BranchGroup createSceneGraph(){
+        // Create the root of the branch graph
+        BranchGroup objRoot = new BranchGroup();
+        
+        TransformGroup objSpin = new TransformGroup();
+        
+        objSpin.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        objRoot.addChild(objSpin);
+        
+       objSpin.addChild(new ColorCube(0.4));
+       
+      Alpha rotationAlpha = new Alpha (-1,4000);
+       
+       RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, objSpin);
+       BoundingSphere bounds = new BoundingSphere();
+       rotator.setSchedulingBounds(bounds);
+       
+       objSpin.addChild(rotator);
+       
+        return objRoot;
+    }
+    
+    
+    public static void main(String[] args){
+        Frame frame = new MainFrame(new HelloJava3Da(), 256,256);
+    }
+}
